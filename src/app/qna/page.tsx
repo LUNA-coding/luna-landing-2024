6e18aa9f974156ee28d5682e5c6241494dd0d491
Seungpyo Suh <sspyorea@gmail.com>
2024-04-16 13:35:39 +0900

    feat: question

--- src/app/qna/page.tsx
+++ src/app/qna/page.tsx
@@ -1,28 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import QnaIntro from "@/components/qna/intro/QnaIntro";
+import QnaListPage from "@/components/qna/list/QnaListPage";
 
 export default function Qna() {
     return (
         <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.left}>
-                    <div className={styles.title}>
-                        <h1>자주 묻는 질문</h1>
-                    </div>
-                    <div className={styles.description}>
-                        루나에 대해서 자주 묻는 질문을 정리했습니다.<br />
-                        아래 질문들에 답변한 유튜브 영상이 있으니, <strong>궁금하시다면<br />
-                        영상을 시청해주세요!</strong>
-                    </div>
-                </div>
-                <div className={styles.right}>
-                    <iframe
-                            src="https://www.youtube-nocookie.com/embed/hIX8CQHqW-M?si=5B9GBHiL_EOXOWuj"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowFullScreen></iframe>
-                </div>
-            </div>
+            <QnaIntro />
+            <QnaListPage />
         </div>
     );
 }
--- src/components/projects/intro/ProjectsIntro.tsx
+++ src/components/projects/intro/ProjectsIntro.tsx
@@ -5,7 +5,7 @@ export default function ProjectsIntro() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <h1>2023년의 루나</h1>
+                <h1>지난 6년간의 루나</h1>
                 <h2>
                     LUNA에서는 어떤 활동들을 통해 사회적 문제들을 해결했을까요?<br/>
                     <strong>스크롤하여 더 확인해보세요!</strong><br/>
