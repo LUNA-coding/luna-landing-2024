6293921b36d92099f8c9ad74b82431dda58265f4
Seungpyo Suh <sspyorea@gmail.com>
2024-04-17 18:02:31 +0900

    feat: home projects

---
+++ src/components/home/intro/HomeIntro.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "./intro.module.css";
+import Scroll from "@/components/scroll/Scroll";
+
+export default function HomeIntro() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.left}>
+                    <div className={styles.title}>
+                        <h2>세상을 비추는 달,</h2>
+                        <h1>LUNA</h1>
+                    </div>
+                    <div className={styles.description}>
+                        <strong>LUNA</strong>는 한국디지털미디어고등학교의 유일한 <strong>IT 소셜벤처 동아리</strong><br />
+                        로 다양한 사회적 문제들을 해결하고 <strong>모두가 함께 살 수 있는 세상을<br />
+                        만들기 위해 노력하고 있습니다.</strong>
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <Image src="/icons/logo.svg" alt="luna" width={500} height={500} />
+                </div>
+            </div>
+            <Scroll />
+        </div>
+    );
+}
---
+++ src/components/home/projects/HomeProjects.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import styles from './projects.module.css';
+import Link from 'next/link';
+
+const ids = ['first', 'second', 'third'];
+
+export default function HomeProjects({ data }: { data: any }) {
+    const projectData = data?.results.map((project: any) => ({
+        name: project.properties.name.title[0].plain_text,
+    }));
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleWheel = (event: WheelEvent) => {
+            setScrollPosition((prevPosition) => prevPosition + event.deltaY);
+        };
+
+        window.addEventListener('wheel', handleWheel);
+
+        return () => {
+            window.removeEventListener('wheel', handleWheel);
+        };
+    }, []);
+
+    const projects = [];
+    for (let i = 0; i < 3; i++) {
+        projects.push(
+            <div key={i} className={styles.projects}>
+                {projectData
+                    ?.filter((_: any, index: number) => index % 3 === i)
+                    .map((project: any, index: number) => (
+                        <div
+                            key={index}
+                            id={styles[ids[index % 3]]}
+                            className={styles.project}
+                            style={{
+                                transform: `translateX(${
+                                    i === 1 ? scrollPosition * 0.5 : -scrollPosition * 0.5
+                                }px)`,
+                            }}
+                        >
+                            <p>{project.name}</p>
+                        </div>
+                    ))}
+            </div>
+        );
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h1 id={styles.left}>루나의 크루원들은 지금까지 자발적으로</h1>
+                <div className={styles.projectZone}>{projects}</div>
+                <div className={styles.right}>
+                    <h1>
+                        <strong>72개의 프로젝트</strong>를 진행하며
+                        <br />
+                        사회적인 가치를 창출해왔습니다.
+                    </h1>
+                    <Link href="/projects">
+                        <h2>루나가 진행한 프로젝트 보러가기</h2>
+                        <img src={'/icons/arrow_forward_ios.svg'} alt="arrow" />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
